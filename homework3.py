06b66817357ce35461d885f6d8b02eec824f1014
PonVit <168406864+PonVit@users.noreply.github.com>
2024-05-07 23:54:07 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,7 @@
+my_string = input('Ввести значение: ')
+str_ = 'Ввести значение: '
+my_string = len(str_)
+print(my_string)
+print('Ввести значение: ' .upper())
+print('Ввести значение: ' .lower())
+print('Ввести значение: ' .replace(' ', '#'))
