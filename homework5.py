a69058168d54cff254c4b1f1601d37ea634b7ac3
PonVit <168406864+PonVit@users.noreply.github.com>
2024-05-15 17:55:26 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['pear', 'apple', 'peach', 'cherry', 'banana']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = ('plum')
+print(my_list)
+my_dict = {'pear': 'груша', 'apple': 'яблоко', 'peach': 'персик', 'cherry': 'черешня', 'banana': 'банан'}
+print(my_dict)
+my_dict['pear'] = 'ГРУША'
+print(my_dict)
+my_dict['grapes'] = 'виноград'
+print(my_dict)
