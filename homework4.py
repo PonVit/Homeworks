87c7e169299843ac5c0e4db69d3bdc531bd28703
PonVit <168406864+PonVit@users.noreply.github.com>
2024-05-22 19:40:42 +0300

    Add files via upload

--- homework4.py
+++ homework4.py
@@ -1,10 +1,9 @@
-immutable_var = tuple([1, 2, 3, True, "String"])
-print(tuple)
-# # immutable_var[0 = 8]    # значение элементов кортежа нельзя изменить, так как кортеж относится к неизменяемому (immutable) типу данных
-# print(tuple)
-immutable_var = tuple([1, 2, 3, True, "String"])
-immutable_var = tuple([1, 2, 25, True, "name"])
+immutable_var = (1, 2, 3, True, "String")
 print(immutable_var)
+# immutable_var[0] = 5    # значение элементов кортежа нельзя изменить, так как кортеж относится к неизменяемому (immutable) типу данных
+immutable_list = [1, 2, 3, True, "String"]
+immutable_list[4] = "name"
+print(immutable_list)
 
 
 
