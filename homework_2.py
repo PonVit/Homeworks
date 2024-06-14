d7d7a0582019839a7aaa5458bb38a2a3f365e3cd
PonVit <168406864+PonVit@users.noreply.github.com>
2024-06-14 19:35:49 +0300

    Add files via upload

---
+++ homework_2.py
@@ -0,0 +1,26 @@
+# Практическое задание по уроку "Базовые структуры данных"
+
+a = len('Пономаренко Виталий Владимирович') # Задача «Длина слова»
+print(a)
+
+first = 9  # Задача «Суммы и разности»
+second = 5
+summa = (first + second)
+diff = (first - second)
+print(summa, diff)
+
+first = 1  # Задача «Среднее арифметическое»
+second = 2
+third = 3
+mean = ((first + second + third) // 3)
+print(mean)
+
+first_string = 'вторник'  # Задача «Простые строчки»
+second_string = 'понедельник'
+print(second_string,first_string)
+
+a = 1   # Задача «Сложная формула»
+b = 2
+c = 3
+f = (((a * b) + (a * c)) ** 3) / 2
+print(f)
